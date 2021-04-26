1d557c0c42e13d54f8f8f4eb57a6c89be9f9fb97
Ubuntu <JeffreySchuitema@outlook.com>
2021-04-26 15:10:46 +0000

    Herokupush Initials/date

--- app/mailboxes/application_mailbox.rb
+++
@@ -1,6 +0,0 @@
-#Elgazar created this is were you put the route for the Email address 
-class ApplicationMailbox < ActionMailbox::Base
-
-  routing RepliesMailbox::MATCHER=> :replies #github email used Rubular
- # routing:all => :replies
-end
